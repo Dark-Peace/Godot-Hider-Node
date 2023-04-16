eb70bc7e20801b8581be0c8c39769fa6dc899692
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-04-16 19:01:24 +0200

    Remove unnecessary stuff

--- addons/Hider Node/Hider_plugin.gd
+++ addons/Hider Node/Hider_plugin.gd
@@ -2,27 +2,16 @@
 extends EditorPlugin
 
 var selection = get_editor_interface().get_selection()
-#var inspector_plugin = preload("res://addons/Hider Node/EditInspectPlugin.gd")
 
 
 func _enter_tree():
 	add_custom_type("Hider", "Node", preload("res://addons/Hider Node/Hider.gd"), preload("res://addons/Hider Node/Hider.png"))
-#	add_custom_type("Hider2D", "Node2D", preload("res://addons/Hider Node/Hider.gd"), preload("res://addons/Hider Node/Hider2D.png"))
-#	add_custom_type("Hider3D", "Node3D", preload("res://addons/Hider Node/Hider.gd"), preload("res://addons/Hider Node/Hider3D.png"))
-#	add_custom_type("HiderGUI", "Control", preload("res://addons/Hider Node/Hider.gd"), preload("res://addons/Hider Node/HiderGUI.png"))
-	
-#	inspector_plugin._selection = get_editor_interface().get_selection()
 	selection.connect("selection_changed", Callable(self,"_on_selection_changed"))
-#	add_inspector_plugin(inspector_plugin)
 	pass
 
 
 func _exit_tree():
 	remove_custom_type("Hider")
-#	remove_custom_type("Hider2D")
-#	remove_custom_type("Hider3D")
-#	remove_custom_type("HiderGUI")
-#	remove_inspector_plugin(inspector_plugin)
 	pass
 
 func _on_selection_changed():
